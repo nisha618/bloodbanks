dae496c4efa9f20d452e9356f36cb50404849924
nisha618 <nisha8427@gmail.com>
2021-06-03 21:33:48 +0545

    second commit

---
+++ province/inside.js
@@ -0,0 +1,136 @@
+document.write(`
+<div class="cards">
+            <div class="card">
+                <div>
+                    <h3>Bhaktapur NRCS Blood Bank,</h3>
+                    <p>Bhaktapur <br> 01-6611661, 01-6612266</p>
+                </div>
+            </div>
+            <div class="card"> 
+                <div>
+                    <h3>Central NRCS Blood Bank,</h3>
+                    <p>Soaltee-Mode<br> 01- 4288485</p>
+                </div>
+            </div>
+
+            <div class="card">
+            <div>
+            <h3>Lalitpur NRCS Blood Bank,</h3>
+            <p>Pulchowk<br>
+            +97715427033</p>
+            </div>
+            </div>
+
+            <div class="card"> 
+            <div>
+            <h3>Nobel Hospital,</h3>
+            <p>Sinamangal<br>
+            01-4110842</p>
+            </div>
+        </div>
+
+        <div class="card"> 
+        <div>
+        <h3>Teaching Hospital,</h3>
+        <p>Maharajgunj, Kathmandu<br>
+        01-44123030, 01-4410911</p>
+        </div>
+    </div>
+
+    <div class="card"> 
+    <div>
+    <h3>Gangalal Hospital,</h3>
+    <p>Bansbari, Kathmandu<br>
+    01-4371322</p>
+    </div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Himal Hospital,</h3>
+<p>Gyaneshwor, Kathmandu<br>
+986-2737316</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Grande Hospital,</h3>
+<p>Dhapasi<br>
+01-5159266</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Prasuti Griha,</h3>
+<p>Thapathali, Kathmandu<br>
+01-4260405</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Nepal Mediciti Hospital,</h3>
+<p>Nakhkhu Ukalo, Lalitpur<br>
+01-4217766</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Bir Hospital,</h3>
+<p>New road gate, Kathmandu<br>
+01-4221119, 01-4221988</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Nepal Police Hospital,</h3>
+<p>Maharajgunj, Kathmandu<br>
+01-4412430</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Civil Hospital,</h3>
+<p>Minbhawan, Kathmandu<br>
+01-4107000</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Patan Hospital,</h3>
+<p>Patan, Lalitpur<br>
+01-5522295</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Birendra Army Hospital,</h3>
+<p>Kathmandu<br>
+01-4271941</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Nepal Medical College,</h3>
+<p>Gokarneswor, Kathmandu<br>
+01-4911008</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Kathmandu Medical College And Teaching Hospital,</h3>
+<p>Sinamangal, Kathmandu<br>
+01-4469064</p>
+</div>
+</div>
+
+`)
---
+++ province/outside.js
@@ -0,0 +1,250 @@
+document.write(`
+<div class="cards">
+            <div class="card">
+                <div>
+                <h3>NRCS Regional BTSC,</h3>
+                <p>Banke, Nepalgunj<br>
+                081 - 520174</p>
+                </div>
+            </div>
+            <div class="card"> 
+                <div>
+                <h3>NRCS Regional BTSC,</h3>
+               <p> Bharatpur, Chitwan<br>
+                056 - 520880</p>
+                </div>
+            </div>
+
+            <div class="card">
+            <div>
+            <h3>NRCS Regional BTSC,</h3>
+           <p> Pokhara, Kaski<br>
+            061 - 521091, 061 - 540191</p>
+            </div>
+            </div>
+           
+            <div class="card"> 
+            <div>
+               <h3>NRCS Regional BTSC,</h3>
+            <p> Rangeli Road, Biratnagar<br>
+             021 - 523326</p>
+            </div>
+        </div>
+
+        <div class="card"> 
+        <div>
+        <h3>Dhulikhel Hospital,</h3>
+        <p>Dhulikhel<br>
+        011 - 490497</p>
+        </div>
+    </div>
+
+    <div class="card"> 
+    <div>
+    <h3>Manipal Teaching Hospital,</h3>
+    <p>Pokhara, Kaski<br>
+    061-526416</p>
+    </div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>BP Koirala Memorial Cancer Hospital,</h3>
+<p>Bharatpur<br>
+056 - 593359</p> 
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Universal College of Medical Sciences,</h3>
+<p>Rupandehi <br>
+071 - 506138</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Janaki Medical College Teaching Hospital,</h3>
+<p>Dhanusha, Janakpur<br>
+041 - 620202</p> 
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Jumla District BTSC,</h3>
+<p>Jumla<br>
+087 - 520036</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Dang District BTSC,</h3>
+<p>Ghorahi, Dang<br>
+082 - 561460</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Bardiya DBTSC,</h3>
+<p>Bardiya<br>
+084 - 420835</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Sunsari District BTSC,</h3>
+<p>Dharan<br>
+025 - 525555</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Jhapa District BTSC,</h3>
+<p>Birtamode, Jhapa<br>
+023 - 541833</p> 
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Ilam District BTSC,</h3>
+<p>Ilam<br>
+027 - 520022</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Panchthar District BTSC,</h3>
+<p>Panchthar<br>
+024 - 570273</p>  
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Butwal District BTSC,</h3>
+<p>Butwal<br>
+071 - 550462</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Siraha District BTSC,</h3>
+<p>Siraha<br>
+033 - 560975</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Terhathum District BTSC ,</h3>
+<p>Myaglung<br>
+026 - 460101</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Makawanpur District BTSC,</h3>
+<p>Makwanpur<br>
+057 - 522977</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Parsa District BTSC,</h3>
+<p>Parsa<br>
+051 - 522504</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Dhanusha District BTSC,</h3>
+<p>Janakpur Provincial Hospital<br>
+041 - 520870,</p> 
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Kavre District BTSC,</h3>
+<p>Banepa, Kavre<br>
+011 - 661431</p>    
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Kanchanpur District BTSC,</h3>
+<p>Kanchapur<br>
+099 - 525588</p> 
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Scheer Memorial Hospital,</h3>
+<p>Banepa, Kavre<br>
+011 - 661111</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Nepalgunj Medical College,</h3>
+<p>Banke<br>
+972 - 3001626</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Tikapur hospital,</h3>
+<p>Kailali<br>
+091 - 560406</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Padama hospital,</h3>
+<p>Kailali<br>
+099 - 550355</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>United Mission Hospital,</h3>
+<p>Palpa<br>
+075 - 520958</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Lamjung Community Hospital</h3>
+<p>Besisahar, Lamjung<br>
+066 - 520183</p>
+</div>
+</div>
+
+<div class="card"> 
+<div>
+<h3>Dr Megh Bahadur Parajuli Community Hospital,</h3>
+<p>Ilam<br> 
+027 - 520417</p>
+</div>
+</div>
+        
+`);
